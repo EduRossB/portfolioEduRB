cb69791aac4bc278735795f097274889bddc4a00
EduRossB <edu_barba@outlook.com>
2023-07-31 19:50:34 +0200

    stack elements added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,62 @@
-import { Container } from 'react-bootstrap';
-import './App.css';
-import Header from './components/Header';
-import MernCard from './components/MernCard';
+import { Container, Row, Col } from "react-bootstrap";
+import "./App.css";
+import Header from "./components/Header";
+import MernCard from "./components/MernCard";
+import logoMongo from './img/logoMongo.png'
+import logoExpress from './img/logoExpress.png'
+import logoReact from './img/logoReact.png'
+import logoNode from './img/logoNode.png'
 
-function App() {
+function App() {  
+  console.log('Hola')
+  const mernStackInfo = [
+    {
+      name: "Mongo DB",
+      color: '#09934E ',
+      image: logoMongo,
+      description:
+        "MongoDB is a NoSQL database that uses a flexible, document-based data model. It stores data in JSON-like documents and is designed to scale horizontally across multiple servers",
+    },
+    {
+      name: "Express",
+      color: '#F3DF1D',
+      image: logoExpress,
+      description:
+        "Express is a minimalistic and fast web framework for Node.js. It simplifies building web applications and APIs with its flexible design and efficient features.",
+    },
+    {
+      name: "React",
+      color: '#00DDFF',
+      image: logoReact,
+      description:
+        "React is a JavaScript library for building UI components and user interfaces. It uses a virtual DOM for efficient rendering and is widely used in modern web development.",
+    },
+    {
+      name: "Node JS",
+      color: '#91C63D',
+      image: logoNode,
+      description:
+        "Node.js is a server-side JavaScript runtime with event-driven architecture, ideal for building scalable applications.",
+    },
+  ];
   return (
     <div className="App">
       <Header />
-      <Container fluid className='mainContainer'>
-        <div className='firstPart my-5'>
-
-          <aside className='halfFirst w-100'><MernCard/></aside>
-        </div>
+      <Container className="mainContainer">
+        <Row className="firstPart my-5 justify-content-center">
+          <Col sm={10} md={6} lg={3} className="halfFirst">
+            <MernCard stackInfo={mernStackInfo[0]} />
+          </Col>
+          <Col sm={10} md={6} lg={3} className="halfFirst">
+            <MernCard stackInfo={mernStackInfo[1]} />
+          </Col>
+          <Col sm={10} md={6} lg={3} className="halfFirst">
+            <MernCard stackInfo={mernStackInfo[2]} />
+          </Col>
+          <Col sm={10} md={6} lg={3} className="halfFirst">
+            <MernCard stackInfo={mernStackInfo[3]} />
+          </Col>
+        </Row>
       </Container>
     </div>
   );
--- src/components/MernCard.jsx
+++ src/components/MernCard.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import "./MernCard.css";
 
-export default function MernCard() {
+export default function MernCard(props) {
+  const { stackInfo } = props;
   return (
-    <div class="card-container w-100">
-      <div class="card">
-        <div class="front-content">
-          <p>MERN</p>
+    <div className="card-container w-100 my-5" >
+      <div className="card">
+        <div className="front-content" style={{ backgroundImage: `url(${stackInfo.image})`, backgroundRepeat: 'no-repeat'}} >
+          <p>{stackInfo.name}</p>
         </div>
-        <div class="content">
-          <p class="heading">MERN</p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipii voluptas ten mollitia
-            pariatur odit, ab minus ratione adipisci accusamus vel est excepturi
-            laboriosam magnam necessitatibus dignissimos molestias.
+        <div
+          className="content"
+          style={{ backgroundColor: stackInfo.color, color: stackInfo.text }}
+        >
+          <p className="heading"></p>
+          <p
+            style={{
+              backgroundColor: "rgba(0, 0, 0, 0.3)",
+              height: "75%",
+              display: "flex",
+              alignItems: "center",
+              padding: "5px",
+              borderRadius: "10%",
+            }}
+          >
+            {stackInfo.description}
           </p>
         </div>
       </div>
