32e60ca927b12f7e887b9294560064319c779994
EduRossB <edu_barba@outlook.com>
2023-08-06 21:45:29 +0200

    card projects added and AboutMe page finished

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 
 export default function Projects() {
   return (
-    <div>
-      <h1 className="text-center">Projects</h1>
-    </div>
+    <Container>
+      <h1 className="subtitle">Projects</h1>
+      <Container className="row text-center">
+        <section className="projectsGroup col-sm-12 col-lg-4">
+          <h4 className="fs-5 w-100">Group Projects</h4>
+          <div className="row w-100 p-0 m-0">
+            <aside className="projectItem col-6">Hola</aside>
+            <aside className="projectItem col-6">Hola</aside>
+          </div>
+        </section>
+        <section className="projectsGroup col-sm-12 col-lg-4">
+          <h4 className="fs-5 w-100">Training Projects</h4>
+          <div className="row w-100 p-0 m-0">
+            <aside className="projectItem col-6">Hola</aside>
+            <aside className="projectItem col-6">Hola</aside>
+          </div>
+        </section>
+        <section className="projectsGroup col-sm-12 col-lg-4">
+          <h4 className="fs-5 w-100">Main Project</h4>
+          <aside className="projectItem"></aside>
+        </section>
+      </Container>
+    </Container>
   );
 }
